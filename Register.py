0d0b02b8e20417b2640d205a573896e890cf16f1
Choprasai123 <112417336+Choprasai123@users.noreply.github.com>
2024-03-28 11:24:17 +0530

    Update Register.py

--- Register.py
+++ Register.py
@@ -2,7 +2,6 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title('My First Streamlit App')
 import pandas as pd
-import plotly.express as px
 from streamlit_option_menu import option_menu
 from PIL import Image
 import time
